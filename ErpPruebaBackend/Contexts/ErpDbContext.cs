b428cb15fa9d5b4ca6884aec394ea1aed324a8f4
Jesus David Muñoz Farfan <jdmunozf97@gmail.com>
2024-03-09 05:00:43 -0500

    first commit

---
+++ ErpPruebaBackend/Contexts/ErpDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ErpSecurity.infrastructure.entities;
+using ErpSecurity.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpPruebaBackend.Contexts
+{
+    public class ErpDbContext : DbContext, SecurityDbContext
+    {
+        public ErpDbContext(DbContextOptions<ErpDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
---
+++ ErpPruebaBackend/Program.cs
@@ -0,0 +1,45 @@
+using ErpIoc;
+using ErpIoc.Options;
+using ErpPruebaBackend.Contexts;
+using ErpPruebaBackend.Controllers;
+using ErpPruebaBackend.Ioc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var configuration = builder.Configuration;
+builder.Services.AddAppDbContext(configuration);
+builder.Services.AddOptions(configuration);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddRepositoryDependencyInjection();
+builder.Services.AddServiceDependencyInjection();
+builder.Services.AddServiceAuthentication(configuration);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseExceptionHandler();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
